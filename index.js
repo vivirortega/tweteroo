9b741ca17de922336ff15ca28f6a400c2cac81db
unverzed <vitoriarortega@gmail.com>
2022-04-22 12:16:17 -0300

    feat: post for signup working

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-app.listen(5000, () => 
-console.log("Server working"))
+let users = [];
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  const user = {
+    username: username,
+    avatar: avatar,
+  };
+
+  users.push(user);
+  res.send("Ok");
+});
+
+app.listen(5000, () => console.log("Server working"));
