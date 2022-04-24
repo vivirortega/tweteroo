a639450009c846d654f322c6c9133db8dc410df9
unverzed <vitoriarortega@gmail.com>
2022-04-24 18:22:26 -0300

    feat: requisition post for tweets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 let users = [];
+let tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
@@ -18,4 +19,21 @@ app.post("/sign-up", (req, res) => {
   res.send("Ok");
 });
 
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  const tweetUser = users.find((user) => user.username === username);
+  
+  const tweetPost = { 
+    username: username,
+    avatar: tweetUser.avatar,
+    tweet: tweet,
+  };
+
+  tweets.push(tweetPost); 
+  console.log(tweetPost)
+  res.send("Ok");
+})
+
+
 app.listen(5000, () => console.log("Server working"));
+
